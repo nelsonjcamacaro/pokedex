02aa8289e42da4be39c3fcd6b6dc4fc5130d0733
nelsonjcamacaro <nelsonjcamacaro@gmail.com>
2023-01-18 18:18:46 -0300

    project configuration

---
+++ app/src/main/java/com/neldev/pokedexapp/ui/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.neldev.pokedexapp.ui.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.neldev.pokedexapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
